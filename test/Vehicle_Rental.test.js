992357cc2bb8be088c30cf865c2179187e7c2907
PensoGlide <eduardo.augusto.santiago@gmail.com>
2021-12-21 04:45:58 +0000

    Added test for contract deployment

--- test/Vehicle_Rental.test.js
+++ test/Vehicle_Rental.test.js
@@ -3,33 +3,37 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-const { abi, evm } = require('../compile');
+//const { abi, evm } = require('../compile');
+//const { interface, bytecode } = require('../compile');
+
+
+const compiledFile = require("../compile"); 
+const interface = compiledFile.abi;
+const bytecode = compiledFile.evm.bytecode.object;
+
+//const compiled_contract = require('../compile');
+//const interface_abi = compiled_contract.output.contracts['Vehicle_Rental.sol']['Vehicle_Rental'].abi;
+//const bytecode = compiled_contract.output.contracts['Vehicle_Rental.sol']['Vehicle_Rental'].evm.bytecode.object;
+
+
 
 let accounts;
-let inbox;
+let vehicle_rental;
+
 
 beforeEach(async () => {
-  // Get a list of all accounts
-  accounts = await web3.eth.getAccounts();
-  inbox = await new web3.eth.Contract(abi)
-    .deploy({
-      data: evm.bytecode.object,
-      arguments: ['Hi there!'],
-    })
-    .send({ from: accounts[0], gas: '1000000' });
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    // Use one of those accounts to deploy the contract
+    vehicle_rental = await new web3.eth.Contract(interface)
+        .deploy({ data: bytecode /** , arguments: [] */ })
+        .send({ from: accounts[0], gas: '1000000'})
 });
 
-describe('Inbox', () => {
-  it('deploys a contract', () => {
-    assert.ok(inbox.options.address);
-  });
-  it('has a default message', async () => {
-    const message = await inbox.methods.message().call();
-    assert.equal(message, 'Hi there!');
-  });
-  it('can change the message', async () => {
-    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
-    const message = await inbox.methods.message().call();
-    assert.equal(message, 'bye');
-  });
+describe('vehicle_rental', () => {
+    it('deploys a contract', () => {
+        console.log(vehicle_rental)
+    });
 });
+
